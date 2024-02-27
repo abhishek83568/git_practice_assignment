2b1505e002064425f36330c225eb918b820f2491
abhishek83568 <abhishek83568@gmail.com>
2024-02-27 15:31:09 +0530

    Code for Prime Number

---
+++ index.js
@@ -0,0 +1,17 @@
+let num=20;
+let count=0;
+for(let i=1;i<=num;i++)
+{
+    if(num%i===0)
+    {
+        count--;
+    }
+}
+
+if(count===2)
+{
+    console.log("It is Prime Number")
+}
+else{
+    console.log("Not a Prime Number")
+}
